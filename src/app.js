dcd574daac1bebf7f93841f6ebd0774e1489d0d0
betolix <betolix@gmail.com>
2022-10-22 00:28:00 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let posPron = Math.floor(Math.random() * pronoun.length);
+  let posAdj = Math.floor(Math.random() * pronoun.length);
+  let posNoun = Math.floor(Math.random() * pronoun.length);
+
+  let domain = pronoun[posPron] + adj[posAdj] + noun[posNoun] + ".com";
+
+  console.log(domain);
   console.log("Hello Rigo from the console!");
 };
